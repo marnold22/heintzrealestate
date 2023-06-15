7b2e0b84b41c72978b2e7ff71f00283e6cbbfd57
Michael <michaelarnold615@gmail.com>
2023-06-15 14:27:41 -0700

    Fixed offcanvas toggle on desktop
    Check for screensize before toggling offCanvas to make sure it is mobile and using the offCanvas menu.

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 const myOffcanvas = new bootstrap.Offcanvas('#offcanvasNavbar');
 
 document.querySelector("#home-anchor-btn").onclick = function () {
-    myOffcanvas.toggle();
+    if($(window).width() <= 991){
+        myOffcanvas.toggle();
+    }
     $([document.documentElement, document.body]).animate({
         scrollTop: $("#home").offset().top
     }, 500);
 }
 document.querySelector("#about-anchor-btn").onclick = function () {
-    myOffcanvas.toggle();
+    if($(window).width() <= 991){
+        myOffcanvas.toggle();
+    }
     $([document.documentElement, document.body]).animate({
         scrollTop: $("#about").offset().top
     }, 500);
 }
 document.querySelector("#listings-anchor-btn").onclick = function () {
-    myOffcanvas.toggle();
+    if($(window).width() <= 991){
+        myOffcanvas.toggle();
+    }
     $([document.documentElement, document.body]).animate({
         scrollTop: $("#listings").offset().top
     }, 500);
 }
 document.querySelector("#contact-anchor-btn").onclick = function () {
-    myOffcanvas.toggle();
+    if($(window).width() <= 991){
+        myOffcanvas.toggle();
+    }
     $([document.documentElement, document.body]).animate({
         scrollTop: $("#contact").offset().top
     }, 500);
